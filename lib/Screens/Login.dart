819a8c6359504a27af164f9ce09a5759b531e291
shedmekhe <mayuresh.s1010@gmail.com>
2022-06-12 00:07:53 +0530

    Done Appearance

--- lib/Screens/Login.dart
+++ lib/Screens/Login.dart
@@ -1,186 +1,3 @@
-// import 'dart:developer';
-//
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:lottie/lottie.dart';
-// import 'package:mini_project_ui/Screens/VerifyEmail.dart';
-// import 'package:mini_project_ui/screens/signup_screen.dart';
-// import 'package:mini_project_ui/screens/first_screen.dart';
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-//
-// class LoginScreen extends StatefulWidget {
-//   const LoginScreen({ Key? key }) : super(key: key);
-//
-//
-//
-//   @override
-//   State<LoginScreen> createState() => _LoginScreenState();
-// }
-//
-// class _LoginScreenState extends State<LoginScreen> {
-//
-//   TextEditingController emailController = TextEditingController();
-//   TextEditingController passwordController = TextEditingController();
-//
-//   void login() async {
-//
-//     String email = emailController.text.trim();
-//     String password = passwordController.text.trim();
-//
-//     if(email == "" || password == "") {
-//       log("Please fill all the fields!");
-//     }
-//     else {
-//
-//       try {
-//         UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
-//         if(userCredential.user != null) {
-//
-//           Navigator.popUntil(context, (route) => route.isFirst);
-//           Navigator.pushReplacement(context, CupertinoPageRoute(
-//               builder: (context) => MyApp()
-//           ));
-//
-//         }
-//       } on FirebaseAuthException catch(ex) {
-//         log(ex.code.toString());
-//       }
-//     }
-//   }
-//   @override
-//   void dispose(){
-//     emailController.dispose();
-//     passwordController.dispose();
-//     super.dispose();
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Colors.teal,
-//         title: Text("User Login"),
-//       ),
-//       body: SingleChildScrollView(
-//         child: Column(
-//           children: <Widget>[
-//           Lottie.asset('assets/lgin.json'),
-//             Padding(
-//               padding: const EdgeInsets.only(top: 60.0),
-//               child: Center(
-//               ),
-//             ),
-//             Padding(
-//               padding: EdgeInsets.all(15),
-//               child: Column(
-//                 children: [
-//
-//
-//                   TextField(
-//                     controller: emailController,
-//                     decoration: InputDecoration(
-//                       prefixIcon: Icon(Icons.mail),
-//                         hintText: "Email Address",
-//                         contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
-//                         border: OutlineInputBorder(
-//                           borderRadius: BorderRadius.circular(10)
-//
-//                       ),
-//                     ),
-//                     ),
-//
-//                     SizedBox(height: 10,),
-//
-//                     TextField(
-//                     controller: passwordController,
-//                     obscureText: true,
-//                     decoration: InputDecoration(
-//                     prefixIcon: Icon(Icons.key),
-//                     hintText: "Password",
-//                     contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
-//                     border: OutlineInputBorder(
-//                     borderRadius: BorderRadius.circular(10)
-//                       ),
-//                     ),
-//                     ),
-//                   SizedBox(
-//                     height: 30,
-//                   ),
-//                     Row(
-//                       mainAxisAlignment: MainAxisAlignment.center,
-//                       children: <Widget>[
-//                           Text("Don't have an account ? "),
-//                         GestureDetector(
-//                           onTap: (){
-//                             Navigator.push(
-//                                 context, MaterialPageRoute(builder: (_) => SignUpScreen()));
-//                         },
-//                           child: Text("Signup",
-//                           style: TextStyle(
-//                               color: Colors.teal,
-//                               fontWeight: FontWeight.bold,
-//                           ),),
-//                         )
-//                       ],
-//                     ),
-//                   SizedBox(
-//                     height: 30,
-//                   ),
-//
-//                     // CupertinoButton(
-//                     // onPressed: () {
-//                     // login();
-//                     // },
-//                     // color: Colors.teal,
-//                     // child: Text("Login"),
-//                     // ),
-//
-//                     // SizedBox(height: 5,),
-//
-//                     // CupertinoButton(
-//                     // onPressed: () {
-//                     //   Navigator.push(context, CupertinoPageRoute(
-//                     //       builder: (context) => SignUpScreen()
-//                     //   ));
-//                     // },
-//                     // child: Text("Create an Account", style: TextStyle( color: Colors.white, fontWeight: FontWeight.bold),)
-//               Container(
-//                 height: 50,
-//                 width: 150,
-//                 decoration: BoxDecoration(
-//                     color: Colors.teal, borderRadius: BorderRadius.circular(20)),
-//                 child: FlatButton(
-//                   onPressed: () {
-//                   //   Navigator.push(
-//                   //       context, MaterialPageRoute(builder: (_) => MyApp()));
-//                   //
-//                   login();
-//                   },
-//                   child: Text(
-//                     'Login',
-//                     style: TextStyle(color: Colors.white, fontSize: 25
-//                     ),
-//
-//                   ),
-//                 ),
-//               ),
-//
-//
-//
-//                   ],
-//
-//
-//               ),
-//             ),
-//
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// -------------------------------------------------------------------------------
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
@@ -259,7 +76,24 @@ class _LoginState extends State<Login> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SizedBox(height: 75),
+            SizedBox(height: 45),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('One',
+                    style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        color: Colors.orange,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 55.0)),
+                SizedBox(width: 5.0),
+                Text('Track',
+                    style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        color: Colors.orangeAccent,
+                        fontSize: 30.0))
+              ],
+            ),
             Form(
               key: _formKey,
               child:SingleChildScrollView(
@@ -267,7 +101,7 @@ class _LoginState extends State<Login> {
                       children: <Widget>[
                               Lottie.asset('assets/password_unlock.json'),
                             Padding(
-                    padding: const EdgeInsets.only(top: 60.0),
+                    padding: const EdgeInsets.only(top: 25.0),
                     child: Center(
                     ),
                   ),
@@ -332,6 +166,9 @@ class _LoginState extends State<Login> {
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           ElevatedButton(
+                            style:ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(Colors.orangeAccent),
+                            ),
                             onPressed: () {
                               // Validate returns true if the form is valid, otherwise false.
                               if (_formKey.currentState!.validate()) {
@@ -356,9 +193,11 @@ class _LoginState extends State<Login> {
                                 ),
                               )
                             },
-                            child: Text(
-                              'Forgot Password ?',
-                              style: TextStyle(fontSize: 14.0),
+                            child: Center(
+                              child: Text(
+                                'Forgot Password ?',
+                                style: TextStyle(fontSize: 16.0,color: Colors.orangeAccent),
+                              ),
                             ),
                           ),
                         ],
@@ -368,7 +207,7 @@ class _LoginState extends State<Login> {
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Text("Don't have an Account? "),
+                          Text("Don't have an Account?",style: TextStyle(fontSize: 18),),
                           TextButton(
                             onPressed: () => {
                               Navigator.pushAndRemoveUntil(
@@ -379,7 +218,7 @@ class _LoginState extends State<Login> {
                                   ),
                                       (route) => false)
                             },
-                            child: Text('Signup'),
+                            child: Text('Signup',style: TextStyle(fontSize: 21,color: Colors.orangeAccent),),
                           ),
                           // TextButton(
                           //   onPressed: () => {
--- lib/Screens/signup_screen.dart
+++ lib/Screens/signup_screen.dart
@@ -1,180 +1,3 @@
-// import 'dart:developer';
-//
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:lottie/lottie.dart';
-// import 'package:mini_project_ui/Screens/Login.dart';
-// import 'package:email_auth/email_auth.dart';
-//
-//
-// class SignUpScreen extends StatefulWidget {
-//   const SignUpScreen({ Key? key }) : super(key: key);
-//
-//   @override
-//   State<SignUpScreen> createState() => _SignUpScreenState();
-// }
-//
-// class _SignUpScreenState extends State<SignUpScreen> {
-//   // final _emailauth = FirebaseAuth.instance;
-//   // EmailAuth emailAuth =  new EmailAuth(sessionName: "OneTrack session");
-//   TextEditingController emailController = TextEditingController();
-//   TextEditingController passwordController = TextEditingController();
-//   TextEditingController cPasswordController = TextEditingController();
-//
-//   void createAccount() async {
-//     String email = emailController.text.trim();
-//     String password = passwordController.text.trim();
-//     String cPassword = cPasswordController.text.trim();
-//
-//     if(email == "" || password == "" || cPassword == "") {
-//       log("Please fill all the details!");
-//     }
-//     else if(password != cPassword) {
-//       log("Passwords do not match!");
-//     }
-//     else {
-//       try {
-//         UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
-//         if(userCredential.user != null) {
-//           Navigator.pop(context);
-//         }
-//       } on FirebaseAuthException catch(ex) {
-//         log(ex.code.toString());
-//       }
-//     }
-//   }
-//
-//   // void sendOtp() async {
-//   //   var result = await emailAuth.sendOtp(
-//   //       recipientMail: emailController.value.text, otpLength: 5
-//   //   );
-//   //   if(result)
-//   //     {
-//   //       print("OTP sent");
-//   //     }else
-//   //       {
-//   //         print("Something went wrong");
-//   //       }
-//   // }
-//   //
-//   // bool verifyOTP() {
-//   //   print(emailAuth.validateOtp(
-//   //       recipientMail: emailController.value.text,
-//   //       userOtp: otpController.value.text));
-//   // }
-//   // emailAuth.validateOtp(
-//   // recipientMail: _emailcontroller.value.text,
-//   // userOtp: _otpcontroller.value.text)
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//
-//       appBar: AppBar(
-//
-//         title: Text("Create account"),
-//       ),
-//       body: SingleChildScrollView(
-//
-//         child: Column(
-//           children: <Widget> [
-//
-//             Padding(
-//               padding: EdgeInsets.all(15),
-//               child: Column(
-//                 children: <Widget> [
-//                 //   SizedBox(
-//                 //     height: 45,
-//                 //   ),
-//                 // SizedBox(
-//                 //   height: 200,
-//                 //   child: Image.asset("images/logo.png",
-//                 //   fit: BoxFit.contain,),
-//                 // ),
-//                 //   SizedBox(
-//                 //     height: 70,
-//                 //   ),
-//                   Lottie.asset('assets/Register.json'),
-//                   SizedBox(height: 10,),
-//                   TextField(
-//                     controller: emailController,
-//                     decoration: InputDecoration(
-//                       prefixIcon: Icon(Icons.mail),
-//                       hintText: "Email Address",
-//                       contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
-//                       border: OutlineInputBorder(
-//                           borderRadius: BorderRadius.circular(10)
-//                       ),
-//                     ),
-//                     ),
-//
-//
-//                   SizedBox(height: 10,),
-//
-//                   TextField(
-//                     controller: passwordController,
-//                     obscureText: true,
-//                     decoration: InputDecoration(
-//                         prefixIcon: Icon(Icons.key),
-//                         hintText: "Password",
-//                         contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
-//                         border: OutlineInputBorder(
-//                             borderRadius: BorderRadius.circular(10)
-//                         ),
-//                     ),
-//                   ),
-//
-//                   SizedBox(height: 10,),
-//
-//                   TextField(
-//                     controller: cPasswordController,
-//                     obscureText: true,
-//                     decoration: InputDecoration(
-//                       prefixIcon: Icon(Icons.vpn_key),
-//                       hintText: "Confirm Password",
-//                       contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
-//                       border: OutlineInputBorder(
-//                           borderRadius: BorderRadius.circular(10)
-//                       ),
-//                     ),
-//                   ),
-//
-//                   SizedBox(height: 100,),
-//
-//                   Container(
-//                     height: 50,
-//                     width: 150,
-//                     decoration: BoxDecoration(
-//                         color: Colors.blue, borderRadius: BorderRadius.circular(20)),
-//                     child: FlatButton(
-//                       onPressed: () {
-//                         //   Navigator.push(
-//                         //       context, MaterialPageRoute(builder: (_) => MyApp()));
-//                         //
-//                         Login();
-//                       },
-//                       child: Text(
-//                         'Register',
-//                         style: TextStyle(color: Colors.white, fontSize: 25
-//                         ),
-//
-//                       ),
-//                     ),
-//                   ),
-//
-//                 ],
-//               ),
-//             )
-//
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-//----------------------------------------------------------------------
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mini_project_ui/Screens/HomeScreen.dart';
@@ -384,7 +207,7 @@ class _SignupState extends State<Signup> {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text("Already have an Account? "),
+                    Text("Already have an Account?",style: TextStyle(fontSize: 16),),
                     TextButton(
                         onPressed: () => {
                           Navigator.pushReplacement(
@@ -397,7 +220,7 @@ class _SignupState extends State<Signup> {
                             ),
                           )
                         },
-                        child: Text('Login'))
+                        child: Text('Login',style: TextStyle(fontSize:21),))
                   ],
                 ),
               )
